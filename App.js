ca3e69b84d630a7356b730372019c69ba9e4c4e5
DaveAbhinav <86666382+DaveAbhinav@users.noreply.github.com>
2022-01-08 16:59:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { render } from 'react-dom';
+import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react/cjs/react.production.min';
+import BottomTabNavigator from './BottomTabNavigator';
+
+export default class App extends Component() {
+  render(){
+  return <BottomTabNavigator/>
+  }
+  
+}
+
